a93b16023656aa55e67809fc45e08f3e1c867c02
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-24 18:03:26 +0100

    Update install_3_root_to_user.sh

--- install_3_root_to_user.sh
+++ install_3_root_to_user.sh
@@ -1,3 +1,5 @@
 echo "+ jump to arch-root install"
-cp install_4.sh /mnt
-sudo arch-chroot /mnt ./install_4.sh
+cp install_4_user.sh /mnt
+cd /mnt
+sudo chmod +x install_4_user.sh
+sudo arch-chroot /mnt ./install_4_user.sh
