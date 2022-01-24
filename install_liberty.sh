365a8b84fa5d14aba9915bb0ce55e33b81ae25c6
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-24 15:47:01 +0100

    Update install_liberty.sh

--- install_liberty.sh
+++ install_liberty.sh
@@ -6,13 +6,4 @@ echo "+ install yay"
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "+ install yay"
 
-git clone https://aur.archlinux.org/yay
-cd yay/
-makepkg -sri
-
-
-echo "+ install font"
-
-yay -S ttf-ms-fonts
