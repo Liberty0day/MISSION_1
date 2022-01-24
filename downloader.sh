be70b128d3950b0a66779b1fad85f5a9dfc9b5db
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-24 14:56:08 +0100

    Create downloader.sh

---
+++ downloader.sh
@@ -0,0 +1,6 @@
+curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install.sh |tee install.sh
+echo "[+] Download install.sh"
+curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_chroot.sh |tee install_chroot.sh
+echo "[+] Download install_chroot.sh"
+curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_liberty.sh |tee install_liberty.sh
+echo "[+] Download install_liberty.sh"
