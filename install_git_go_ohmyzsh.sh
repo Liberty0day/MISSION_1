a4365183d46cc2733e583ee31d0fadf5816f4fc9
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-24 17:13:59 +0100

    Delete install_git_go_ohmyzsh.sh

--- install_git_go_ohmyzsh.sh
+++
@@ -1,9 +0,0 @@
-echo "+ install git"
-
-su liberty -c "pacman -Sy --noconfirm git"
-su liberty -c "pacman -Sy --noconfirm go"
-
-echo "+ install ohmyzsh"
-
-su liberty -c "sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)""
-
