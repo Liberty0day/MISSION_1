33cd81da719d67dd1cff30ec220c38e55bddfb8b
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-24 15:13:51 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ echo "+ make cryto for boot partition"
 
 cryptsetup --type luks2 --pbkdf argon2id --cipher serpent-xts-plain64 --key-size 512 --hash sha512 --use-random --verify-passphrase luksFormat /dev/archlvm/root
 
-read
+
 echo "+ test your crypto"
 
 cryptsetup open /dev/archlvm/root root
