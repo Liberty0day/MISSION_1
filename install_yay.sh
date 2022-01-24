22318f762f1a7a1eed045b57a1a1784091b0bb8c
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-24 16:25:05 +0100

    Update install_yay.sh

--- install_yay.sh
+++ install_yay.sh
@@ -2,7 +2,7 @@ echo "+ install yay"
 
 su liberty -c "git clone https://aur.archlinux.org/yay"
 su liberty -c "cd yay/"
- su liberty -c "makepkg -sri"
+su liberty -c "makepkg -sri"
 
 
 echo "+ install font"
