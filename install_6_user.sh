10a7461c088a79a64772c174ca996c13495c316b
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-25 19:11:02 +0100

    Update install_6_user.sh

--- install_6_user.sh
+++ install_6_user.sh
@@ -1,19 +1,13 @@
-echo "+ install git"
-
-su liberty -c "pacman -Sy --noconfirm git"
-su liberty -c "pacman -Sy --noconfirm go"
-
 echo "+ install ohmyzsh"
-
-su liberty -c "sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)""
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "+ install yay"
 
-su liberty -c "git clone https://aur.archlinux.org/yay"
-su liberty -c "cd yay/"
-su liberty -c "makepkg -sri"
+git clone https://aur.archlinux.org/yay
+cd yay/
+makepkg -sri
 
 
 echo "+ install font"
 
-su liberty -c "yay -S ttf-ms-fonts"
+yay -S ttf-ms-fonts
