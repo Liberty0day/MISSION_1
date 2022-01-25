8c7f5c57f8ee2e6e2b3fb7d00325e725453c751d
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-25 20:08:55 +0100

    Update install_3_root_to_user.sh

--- install_3_root_to_user.sh
+++ install_3_root_to_user.sh
@@ -1,5 +1,8 @@
 echo "+ jump to arch-root install"
+cp fstab /mnt/home
+cp crypttab /mnt/home
 cp install_4_user.sh /mnt/home
+
 arch-chroot /mnt sh /home/install_4_user.sh
 rm /mnt/home/install_4_user.sh
 echo 'Setup Complete!'
