54280094af6e0e576eb26cfcdccd5ac89a4921a0
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-25 21:19:19 +0100

    Update install_4.0_user.sh

--- install_4.0_user.sh
+++ install_4.0_user.sh
@@ -97,20 +97,20 @@ echo "+ need uncrypt partition when you boot need edit file"
 
 echo "+ fastab"
 
-cat <<EOF >> /mnt/etc/crypttab
+  cat <<EOF >> /mnt/etc/crypttab
     home /dev/mapper/archlvm-home  /etc/luks-keys/home
     swap /dev/mapper/archlvm-swap  /dev/urandom  swap,cipher=serpent-xts-plain64,size=512
     tmp  /dev/mapper/archlvm-tmp   /dev/urandom  tmp,cipher=serpent-xts-plain64,size=512
 EOF
 
-cat /mnt/etc/crypttab
+  cat /mnt/etc/crypttab
 
 echo "+ crypttab"
 
-cat <<EOF >> /mnt/etc/fstab
+  cat <<EOF >> /mnt/etc/fstab
     /dev/mapper/tmp   /tmp   tmpfs  defaults  0  0
     /dev/mapper/swap  none   swap   sw  0  0
     /dev/mapper/home  /home  xfs    defaults  0  2
 EOF
 
-cat /mnt/etc/fstab
+  cat /mnt/etc/fstab
