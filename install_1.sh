d7adb6e9ed0464fed8521d8300192dd223386dbc
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-25 19:54:27 +0100

    Update install_1.sh

--- install_1.sh
+++ install_1.sh
@@ -2,8 +2,8 @@ curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_2_root
 curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_3_root_to_user.sh | tee install_3_root_to_user.sh
 curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_4_user.sh | tee install_4_user.sh
 curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_5_user.sh | tee install_5_user.sh
-curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_6_user.sh | tee install_6_user.sh
-
+curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_6.0_user.sh | tee install_6.0_user.sh
+curl https://raw.githubusercontent.com/Liberty0day/MISSION_1/main/install_6.1_user.sh | tee install_6.1_user.sh
 
 #zsh install_2_root.sh
 #zsh install_3_root_to_user.sh
