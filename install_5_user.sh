283abf8de28ee8c9bd375005652065f0c26988ae
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-25 19:08:18 +0100

    Update install_5_user.sh

--- install_5_user.sh
+++ install_5_user.sh
@@ -1,20 +1,21 @@
 echo "+ make user"
-useradd -mG wheel -s /usr/bin/zsh -c "Liberty" liberty
-passwd liberty
+arch-chroot /mnt useradd -mG wheel -s /usr/bin/zsh -c "Liberty" liberty
+arch-chroot /mnt passwd liberty
+
 echo "+ make user sudoers remove # %wheel ALL=(ALL) ALL in visudo /etc/sudoers"
-sudo sed -i '82 s/^#//' /etc/sudoers
+arch-chroot /mnt sudo sed -i '82 s/^#//' /etc/sudoers
 echo "+ enable time ntp and deamon cron and syslog-ng"
-pacman -Sy --noconfirm cronie ntp
-systemctl enable cronie
-systemctl enable ntpd
-systemctl enable syslog-ng@default
+arch-chroot /mnt pacman -Sy --noconfirm cronie ntp
+arch-chroot /mnt systemctl enable cronie
+arch-chroot /mnt systemctl enable ntpd
+arch-chroot /mnt systemctl enable syslog-ng@default
 echo "+ enable tlp for batterie"
-pacman -S --noconfirm tlp && systemctl enable tlp
+arch-chroot /mnt pacman -S --noconfirm tlp && systemctl enable tlp
 echo "+ install driver for graphic card"
-pacman -S --noconfirm xorg-{server,xinit,apps} xdg-user-dirs xf86-video-intel mesa lib32-mesa
+arch-chroot /mnt pacman -S --noconfirm xorg-{server,xinit,apps} xdg-user-dirs xf86-video-intel mesa lib32-mesa
 echo "+ install cpu"
-pacman -Sy --noconfirm intel-ucode
+arch-chroot /mnt pacman -Sy --noconfirm intel-ucode
 echo "+ install audio codec"
-pacman -S --noconfirm gst-libav gst-plugins-{base,good,bad,ugly}
+arch-chroot /mnt pacman -S --noconfirm gst-libav gst-plugins-{base,good,bad,ugly}
 echo "+ install fonts"
-pacman -S --noconfirm xorg-fonts-type1 gsfonts sdl_ttf ttf-{dejavu,bitstream-vera,liberation} noto-fonts-{cjk,emoji,extra}
+arch-chroot /mnt pacman -S --noconfirm xorg-fonts-type1 gsfonts sdl_ttf ttf-{dejavu,bitstream-vera,liberation} noto-fonts-{cjk,emoji,extra}
