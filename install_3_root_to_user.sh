ac618fcca1ec7b1084a0731359092784003ad93e
Liberty0day <98103103+Liberty0day@users.noreply.github.com>
2022-01-27 20:16:27 +0100

    Update install_3_root_to_user.sh

--- install_3_root_to_user.sh
+++ install_3_root_to_user.sh
@@ -2,7 +2,7 @@ echo "+ jump to arch-root install"
 
 cp install_4.0_user.sh /mnt/home
 
-arch-chroot /mnt sh /home/install_4.0_user.sh
+arch-chroot /mnt /home/install_4.0_user.sh
 #rm /mnt/home/install_4.0_user.sh
 echo 'Setup Complete!'
 echo 'type "reboot" and remove installation media.'
